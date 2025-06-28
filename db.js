ad507394ab4d7a3411badcbf189c15030b5c44ef
Oleksii Zozulenko <keemdek@gmail.com>
2025-06-28 11:13:51 +0300

    update local db name

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const DB_PATH = path.join(__dirname, 'perf-tester.db');
+const DB_PATH = path.join(__dirname, 'perf-tester.sqlite');
 const db = new sqlite3.Database(DB_PATH);
 
 // Create table if not exists
